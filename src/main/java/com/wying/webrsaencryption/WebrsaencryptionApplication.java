124d5d530089683c2c3d474348aaa50246dc8a1d
gaom <gaom@bsoft.com.cn>
2020-06-12 22:10:04 +0800

    spring-boot dev

--- src/main/java/com/wying/webrsaencryption/WebrsaencryptionApplication.java
+++ src/main/java/com/wying/webrsaencryption/WebrsaencryptionApplication.java
@@ -2,6 +2,8 @@ package com.wying.webrsaencryption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class WebrsaencryptionApplication {
