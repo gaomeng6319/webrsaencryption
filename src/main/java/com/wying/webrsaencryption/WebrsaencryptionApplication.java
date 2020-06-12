5c0d5cbeb1d80f7444cfb6ef236eae14d8784305
gaom <gaom@bsoft.com.cn>
2020-06-12 21:06:06 +0800

    spring-boot init

---
+++ src/main/java/com/wying/webrsaencryption/WebrsaencryptionApplication.java
@@ -0,0 +1,13 @@
+package com.wying.webrsaencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrsaencryptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebrsaencryptionApplication.class, args);
+    }
+
+}
